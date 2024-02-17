67715297c46f679a1266d002c9089b3f73d29a4e
anantqa <tiwari_anant@outlook.com>
2024-02-17 17:09:32 +0530

    Intial commit

---
+++ GetService.ps1
@@ -0,0 +1,9 @@
+﻿
+$ServiceName = "Security Center"
+$status = Get-Service -DisplayName $ServiceName
+if($status.Status -eq "Running"){
+Write-Host "Service Is In Running State" -ForegroundColor Green
+}
+else{
+Write-Host "Service Is Stopped"
+}
