b7068170f04a5647a3b0f503228f423d92dcd09b
anantqa <tiwari_anant@outlook.com>
2024-02-17 17:46:45 +0530

    Added service restart command in script

--- GetService.ps1
+++ GetService.ps1
@@ -1,4 +1,4 @@
-﻿
+﻿#change Security Center with the service name you want check status for
 $ServiceName = "Security Center"
 $status = Get-Service -DisplayName $ServiceName
 if($status.Status -eq "Running"){
@@ -6,4 +6,6 @@ Write-Host "Service Is In Running State" -ForegroundColor Green
 }
 else{
 Write-Host "Service Is Stopped"
+#remove # from below line in case you want to restart the service in case of not running
+#Restart-Service -Name $status.Name -Force
 }
